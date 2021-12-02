71a133a46fb1f4bc8a2f201604ffb808a511e543
ajaykoka <ajaykoka1704@gmail.com>
2021-12-02 13:12:46 +0530

    v1

---
+++ script.js
@@ -0,0 +1,43 @@
+// Js 
+
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = [] // initialise
+
+getData() //upon
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
+async function getData() {
+    const res = await fetch('http://randomuser.me/api?results=50')
+
+    const {results} = await res.json()
+
+    result.innerHTML = '' //clear
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+        <img src="${user.picture.medium}" alt="" >
+        <div class="user-info">
+            <h4>${user.name.first}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `
+        result.appendChild(li)
+    });
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase()))
+        {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
+}
